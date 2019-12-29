e50e6329152ff1683b2da00e7162536bc4230ee6
yusufbkr <f.yusufbakir@gmail.com>
2019-12-29 08:46:19 +0300

    added styles

--- pages/_document.js
+++ pages/_document.js
@@ -3,23 +3,34 @@ import React from 'react';
 import { AppRegistry } from 'react-native';
 import config from '../now.json';
 
+const normalizeNextElements = `
+  #__next {
+    display: flex;
+    flex-direction: column;
+    height: 100%;
+  }
+`;
+
 export default class MyDocument extends Document {
   static async getInitialProps(ctx) {
     const { renderPage } = ctx;
     AppRegistry.registerComponent(config.name, () => Main);
     const { getStyleElement } = AppRegistry.getApplication(config.name);
     const page = renderPage();
-    const styles = [getStyleElement()];
+    const styles = [
+      <style dangerouslySetInnerHTML={{ __html: normalizeNextElements }} />,
+      getStyleElement()
+    ];
     return { ...page, styles: React.Children.toArray(styles) };
   }
 
   render() {
     return (
-      <html lang="en">
+      <html lang="en" style={{ height: '100%' }}>
         <Head>
           <meta name="viewport" content="width=device-width, initial-scale=1" />
         </Head>
-        <body>
+        <body style={{ height: '100%', overflow: 'hidden' }}>
           <Main />
           <NextScript />
         </body>
