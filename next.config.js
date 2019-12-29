b0569236a6d08cf80ea0bcd5008afb24a071b2b0
yusufbkr <f.yusufbakir@gmail.com>
2019-12-29 09:34:58 +0300

    fix

--- next.config.js
+++ next.config.js
@@ -15,7 +15,6 @@ const nextConfiguration = {
   webpack: (config, { defaultLoaders }) => {
     config.resolve.alias = {
       ...(config.resolve.alias || {}),
-      // Transform all direct `react-native` imports to `react-native-web`
       'react-native$': 'react-native-web'
     };
     config.resolve.extensions.push('.web.js', '.web.ts', '.web.tsx');
